35692a6d0f8e1e9f344b7c543ba7a5f9bf4c0522
cbursey <32469718+cbursey@users.noreply.github.com>
2017-12-22 22:34:49 -0600

    Add files via upload

--- core.js
+++ core.js
@@ -2,7 +2,6 @@ var zombieApp = angular.module('zombieApp', ['ngResource']);
 
 zombieApp.controller('mainController', ['$scope', '$resource',
     function ($scope, $resource) {
-
         window.twttr = (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0],
             t = window.twttr || {};
@@ -19,71 +18,223 @@ zombieApp.controller('mainController', ['$scope', '$resource',
             return t;
         }(document, "script", "twitter-wjs"));
 
-        $scope.selectedTime = "PM";
-        $scope.timeOptions = ["AM", "PM"];
-
+        $scope.feedStarted = false;
         $scope.inputHashtag = "";
-        $scope.inputFreq = 10; //tweets per second
-        $scope.inputRuntime = 30; // length of episode
-        $scope.inputStart = "201712121430"; 
-        $scope.inputEnd = "201712121500";
-        $scope.embedURL = "";
+        $scope.inputDate = new Date();
+        var today = new Date();      
+        $scope.inputStartTime = new Date(today.getFullYear(), today.getMonth(), 
+                                    today.getDate(), today.getHours() - 1, today.getMinutes());
+        $scope.inputEndTime = new Date(today.getFullYear(), today.getMonth(), 
+                                    today.getDate(), today.getHours(), today.getMinutes()); 
+        $scope.errorMessage = "";
+        $scope.timerDate = "";
+        $scope.timerTime = "";
 
-        var getStartDate = function() {
+        var startDate = new Date();
+        var endDate = new Date();
+        var utcStartString = "";
+        var utcEndString = "";
+        var tweetQueue = [];
 
-        }
+        var processInputs = function() {
+            if ($scope.inputHashtag === "") {
+                $scope.errorMessage = "Please enter a hashtag to search.";
+                return false;
+            }
+            if (isNaN($scope.inputDate.getTime())) {
+                $scope.errorMessage = "Please enter a valid date.";
+                return false;
+            }
+            if (isNaN($scope.inputStartTime.getTime())) {
+                $scope.errorMessage = "Please enter a valid start time.";
+                return false;
+            }
+            if (isNaN($scope.inputEndTime.getTime())) {
+                $scope.errorMessage = "Please enter a valid end time.";
+                return false;
+            }
+            if ($scope.inputEndTime.getTime() <= $scope.inputStartTime.getTime()) {
+                $scope.errorMessage = "Start time must be before end time.";
+                return false;
+            } 
+            //input is correct, now formatting dates.
+            startDate = new Date(
+                $scope.inputDate.getFullYear(),
+                $scope.inputDate.getMonth(),
+                $scope.inputDate.getDate(),
+                $scope.inputStartTime.getHours(),
+                $scope.inputStartTime.getMinutes());
+            endDate = new Date(
+                $scope.inputDate.getFullYear(),
+                $scope.inputDate.getMonth(),
+                $scope.inputDate.getDate(),
+                $scope.inputEndTime.getHours(),
+                $scope.inputEndTime.getMinutes());
 
+            //YYYYMMDDhhmm (UTC), formatted for twitter API
+
+            var utcStartMonth = (startDate.getUTCMonth() + 1).toString();
+            var utcEndMonth = (endDate.getUTCMonth() + 1).toString();
+            utcStartMonth = utcStartMonth.length === 1 ? ("0" + utcStartMonth) : utcStartMonth;
+            utcEndMonth = utcEndMonth.length === 1 ? ("0" + utcEndMonth) : utcEndMonth;
+
+            var utcStartDay = startDate.getUTCDate().toString();
+            var utcEndDay = endDate.getUTCDate().toString();
+            utcStartDay = utcStartDay.length === 1 ? ("0" + utcStartDay) : utcStartDay;
+            utcEndDay = utcEndDay.length === 1 ? ("0" + utcEndDay) : utcEndDay;
+
+            var utcStartHr = startDate.getUTCHours().toString();
+            var utcEndHr = endDate.getUTCHours().toString();
+            utcStartHr = utcStartHr.length === 1 ? ("0" + utcStartHr) : utcStartHr;
+            utcEndHr = utcEndHr.length === 1 ? ("0" + utcEndHr) : utcEndHr;
+
+            var utcStartMin = startDate.getUTCMinutes().toString();
+            var utcEndMin = endDate.getUTCMinutes().toString();
+            utcStartMin = utcStartMin.length === 1 ? ("0" + utcStartMin) : utcStartMin;
+            utcEndMin = utcEndMin.length === 1 ? ("0" + utcEndMin) : utcEndMin;
+
+            utcStartString = startDate.getUTCFullYear().toString() +
+                            utcStartMonth + utcStartDay +
+                            utcStartHr + utcStartMin;
+            utcEndString = endDate.getUTCFullYear().toString() +
+                            utcEndMonth + utcEndDay +
+                            utcEndHr + utcEndMin;
+            $scope.errorMessage = "";
+            $scope.inputHashtag = "#" + $scope.inputHashtag;
+            return true;
+        };
+
+        var tweetTimer = function(){
+            console.log('tweetQueue dates:');
+            for (var i = 0; i < tweetQueue.length; i++){
+                console.log(tweetQueue[i]);
+            }
+            var embedIndex = tweetQueue.length - 1;
+            var displayDate = startDate;
+            var month = displayDate.getMonth() + 1;             
+            $scope.timerDate = 
+                month + "/" + 
+                displayDate.getDate() + "/" + 
+                displayDate.getFullYear();
+            $scope.feedStarted = true;
+            var timer = setInterval(function(){
+                var meridian = "AM";
+                var hours = displayDate.getHours();
+                var minutes = displayDate.getMinutes();
+                var seconds = displayDate.getSeconds();
+
+                var utcHours = displayDate.getUTCHours();
+                var utcMinutes = displayDate.getUTCMinutes();
+                var utcSeconds = displayDate.getUTCSeconds();
+
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                utcHours = utcHours < 10 ? "0" + utcHours : utcHours;
+                utcMinutes = utcMinutes < 10 ? "0" + utcMinutes : utcMinutes;
+                utcSeconds = utcSeconds < 10 ? "0" + utcSeconds : utcSeconds;
+
+                var currUTC = utcHours + ":" + utcMinutes + ":" + utcSeconds;
+                if (hours > 12){
+                    hours = hours - 12;
+                    meridian = "PM";
+                }
+                $scope.timerTime = 
+                    hours + ":" +
+                    minutes + ":" +
+                    seconds + " " + 
+                    meridian;
+                $scope.$apply();
+                //finds the most popular tweet in a given second
+                var secondGroup = [];
+                while (embedIndex >= 0 && currUTC === tweetQueue[embedIndex].date){
+                    secondGroup.push(tweetQueue[embedIndex]);
+                    embedIndex--;
+                }
+                console.log("embed index:", embedIndex);
+                if (secondGroup.length > 0){ embed(secondGroup) };
+                displayDate.setSeconds(displayDate.getSeconds() + 1);
+                if (embedIndex < 0 || displayDate.getTime() >= endDate.getTime()) { 
+                    clearInterval(timer); 
+                }
+            }, 1000);
+        };
         $scope.testDates = function(){
-            console.log($scope.inputStart);
-            console.log($scope.inputEnd);
-        }
+            if (processInputs()){
+                console.log('UTC Start:', utcStartString);
+                console.log('UTC End:', utcEndString);
+                tweetTimer();
+            }
+        };
 
         var TweetResource = $resource('/fetchTweets', null, {
             query: {method: 'POST', isArray: true, cancellable: true}
         });
 
-        var EmbedResource = $resource('/fetchEmbed:url');
+        var EmbedResource = $resource('/fetchEmbed:id');
 
-        $scope.embed = function() {
-            var embedHTML = EmbedResource.get({url: $scope.embedURL}, function(){
+        //looks for most popular tweet generated in one second, embeds to page.
+        var lastTweet = "";
+        var embed = function(secondGroup) {
+            var mostPopular = secondGroup[0].id;
+            var mostFavs = secondGroup[0].favorite_count;
+            for (var i = 0; i < secondGroup.length; i++){
+                if (secondGroup[i].favorite_count > mostFavs){
+                    mostPopular = secondGroup[i].id;
+                    mostFavs = secondGroup[i].favorite_count;
+                }
+            }
+            var embedHTML = EmbedResource.get({id: mostPopular}, function(){
                 var interface = document.getElementById("interface");
                 var centeredHTML = embedHTML.html.substring(33, embedHTML.html.length - 1);
                 centeredHTML = "<blockquote class=\"twitter-tweet tw-align-center\"" + centeredHTML;
                 var newTweet = document.createElement("div");
                 newTweet.innerHTML += centeredHTML;
-               // console.log(centeredHTML);
-                interface.appendChild(newTweet);
+                if (lastTweet === ""){
+                    interface.appendChild(newTweet);
+                } else {
+                    interface.insertBefore(newTweet, lastTweet);
+                }
+                lastTweet = newTweet;
                 twttr.widgets.load(document.getElementById(interface));
             });
         }; 
-
         $scope.fetchData = function() {
-            console.log($scope.selectedTime);
-            var startDate = $scope.inputStart; //TODO: format as YYYYMMDDhhmm (UTC)
-            var endDate = $scope.inputEnd; //YYYYMMDDhhmm (UTC)
-            var num = ($scope.inputRuntime * 60) / $scope.inputRuntime; //number of tweets to fetch
-
-            //TODO: DELETE the following block, only for testing
-            $scope.inputHashtag = "dog";
-            startDate = "201712121430";
-            endDate = "201712121500";
-            num = 10;
-            //END deleting
-
-
+            if (!processInputs()){ return; }
             var body = {
                 hashtag: $scope.inputHashtag,
-                fromDate: startDate,
-                toDate: endDate,
-                count: num,
+                fromDate: utcStartString,
+                toDate: utcEndString,
             };
-            var tweets = TweetResource.save({}, body, function(){
-                console.log(tweets);
-                $scope.embedID = tweets.results[0].id_str;
-                console.log($scope.embedID);
-                $scope.embed();
-
-            });
+            var next = true;
+            var tweetObjs = [];
+            var request = function(reqBody) {
+                var tweets = TweetResource.save({}, reqBody, function(){
+                    console.log(tweets);
+                    for (var i = 0; i < tweets.results.length; i++){
+                        var createdDate = tweets.results[i].created_at.substring(11, 19);
+                        var tweetObj = {date: createdDate, 
+                                        id: tweets.results[i].id_str,
+                                        favs: tweets.results[i].favorite_count};
+                        tweetQueue.push(tweetObj);
+                    }
+                    if (tweets.next){
+                        body = {
+                            hashtag: $scope.inputHashtag,
+                            fromDate: utcStartString,
+                            toDate: utcEndString,
+                            next: tweets.next
+                        };
+                        request(body);
+                    } else {
+                        tweetTimer();
+                    }
+                }, function(err){
+                    console.error(err);
+                });
+                return;
+            }
+            request(body);
         };
     }
 ]);
--- server.js
+++ server.js
@@ -1,69 +1,62 @@
 var express = require('express');
-var app = express();                               // create our app w/ express
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-
-
-app.use(express.static(__dirname));        
-app.use(morgan('dev'));                                         // log every request to the console          // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-    // listen (start app with node server.js) ======================================
-
+var app = express();                               
+var morgan = require('morgan'); 
+var bodyParser = require('body-parser'); 
 var Twitter = require('twitter-node-client').Twitter;
-
-
 var twitter = new Twitter();
 
+app.use(express.static(__dirname));        
+app.use(morgan('dev'));                                       
+app.use(bodyParser.json());                                     
 
 app.get('/', function (req, res) {
     res.send('Simple web server of files from ' + __dirname);
 });
 
-app.get('/fetchEmbed:url', function (req, res) {
-	twitter.getCustomApiCall('/statuses/oembed.json', {id: req.params.url},
+//fetches oembed HTML  to embed tweets in feed
+app.get('/fetchEmbed:id', function (req, res) {
+	twitter.getCustomApiCall('/statuses/oembed.json', {id: req.params.id},
 		function (err, response, body) {
-    		console.log('/FETCHEMBED ERR:', req.params.url, err);
-    		res.status(400).send('/FETCHEMBED ERR');
+    		console.error('/fetchEmber error:', err);
+    		res.status(400).send('/fetchEmbed error', err);
 		}, 
 		function (data) {
-			console.log('heres the data');
-			console.log(data);
-			console.log('ok');
+			console.log('/fetchEmbed success');
 			res.send(data);
 		}
 	);
 });
 
-/*app.post('/fetchTweets', function (req, res) {
-	console.log('fetchin');
-	twitter.getUserTimeline({ screen_name: 'FiveThirtyEight', count: '10'},
-	function (err, response, body) {
-    	console.log('/FETCHTWEETS ERR:', err);
-    	res.status(400).send('/FETCHTWEETS ERR');
-	}, function (data) {
-		console.log(data);
-		res.end(data);
-	});
-});*/
-
+//fetches tweets data based on parameters 
 app.post('/fetchTweets', function (req, res) {
 	console.log('fetchin');
-	twitter.getCustomApiCall('/tweets/search/30day/dev.json', {
-		"query": "#puppy",
-		"fromDate":"201712121430",
-		"toDate":"201712121500",
-		"maxResults": 10
-	},
-	function (err, response, body) {
-    	console.log('/FETCHTWEETS ERR:', err);
-    	res.status(400).send('/FETCHTWEETS ERR');
-	}, function (data) {
-		console.log(data);
-		res.end(data);
-	});
+	var body = {};
+	if (req.body.next){
+		body = {
+			"query": req.body.hashtag,
+			"fromDate": req.body.fromDate,
+			"toDate": req.body.toDate,
+			"maxResults": 100,
+			"next": req.body.next
+		};
+	} else {
+		body = {
+			"query": req.body.hashtag,
+			"fromDate": req.body.fromDate,
+			"toDate": req.body.toDate,
+			"maxResults": 100
+		};
+	}
+	twitter.getCustomApiCall('/tweets/search/30day/dev.json', body,
+		function (err, response, body) {
+    		console.error('/fetchTweets error:', err);
+    		res.status(400).send('/fetchTweets error:', err);
+		}, function (data) {
+			console.log('/fetchTweets success');
+			res.end(data);
+		}
+	);
 });
-
-
 
 var server = app.listen(3000, function () {
     var port = server.address().port;
